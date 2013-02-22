bc602961ec25f0fa5f7cb58890e69bd1cf3c090c
timsegraves <timsegraves@gmail.com>
2013-02-22 15:41:57 -0700

    Adding underscore method

--- lib/recursive_open_struct.rb
+++ lib/recursive_open_struct.rb
@@ -67,7 +67,7 @@ class RecursiveOpenStruct < OpenStruct
   end
 
   def underscore(key)
-    key.gsub(/::/, '/').
+    key.to_s.gsub(/::/, '/').
     gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
     gsub(/([a-z\d])([A-Z])/,'\1_\2').
     tr("-", "_").
